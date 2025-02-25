7306a073ae810d85a8e691deff8a421de3e7715b
Andy <andy-tux@aon.at>
2025-02-25 15:27:10 +0100

    Added folder Services and made UserService

---
+++ PermissionManagementApp/Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PermissionManagementApp.Data;
+using PermissionManagementApp.Data.Models;
+
+namespace PermissionManagementApp.Services
+{
+	public class UserService
+	{
+		private readonly AppDbContext dbContext;
+
+		public UserService(AppDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		public async Task<List<User>> GetAllUsersAsync()
+		{
+			return await dbContext.Users.Include(u=> u.UserGroups).ToListAsync();
+		}
+
+		public async Task<User?> GetUserByIdAsync(int id)
+		{
+			return await dbContext.Users.Include(u=> u.UserGroups).FirstOrDefaultAsync(u=> u.Id == id);
+		}
+
+		public async Task AddUserAsync(User user)
+		{
+			await dbContext.Users.AddAsync(user);
+			await dbContext.SaveChangesAsync();
+		}
+
+		public async Task DeleteUserAsync(int userId)
+		{
+			var user = await GetUserByIdAsync(userId);
+
+			if(user != null)
+			{
+				dbContext.Users.Remove(user);
+				await dbContext.SaveChangesAsync();
+			}
+		}
+	}
+}
