c261bd035c83c4613510b87c745bb74eab9701fe
Andy <andy-tux@aon.at>
2025-02-27 09:14:42 +0100

    Added PermissionService

---
+++ PermissionManagementApp/Services/PermissionService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PermissionManagementApp.Data;
+using PermissionManagementApp.Data.Models;
+
+namespace PermissionManagementApp.Services
+{
+	public class PermissionService
+	{
+		private readonly AppDbContext dbContext;
+
+		public PermissionService(AppDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		public async Task<List<Permission>> GetAllPermissionsAsync()
+		{
+			return await dbContext.Permissions.ToListAsync();
+		}
+
+		public async Task<Permission?> GetPermissionByIdAsync(int id)
+		{
+			return await dbContext.Permissions.FirstOrDefaultAsync(x => x.Id == id);
+		}
+
+		public async Task AddPermissionAsync(Permission permission)
+		{
+			await dbContext.Permissions.AddAsync(permission);
+			await dbContext.SaveChangesAsync();
+		}
+
+		public async Task UpdatePermissionAsync(Permission permission)
+		{
+			dbContext.Permissions.Update(permission);
+			await dbContext.SaveChangesAsync();
+		}
+
+		public async Task DeletePermissionAsync(int id)
+		{
+			var permission = await GetPermissionByIdAsync(id);
+
+			if (permission != null)
+			{
+				dbContext.Permissions.Remove(permission);
+				await dbContext.SaveChangesAsync();
+			}
+		}
+	}
+}
