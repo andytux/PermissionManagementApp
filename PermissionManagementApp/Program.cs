e2da9dba695b596b089411f3a81b752c6e2a7754
Andy <andy-tux@aon.at>
2025-02-27 09:16:17 +0100

    Added Services in program.cs

--- PermissionManagementApp/Program.cs
+++ PermissionManagementApp/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using PermissionManagementApp.Components;
 using PermissionManagementApp.Data;
+using PermissionManagementApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<GroupService>();
+builder.Services.AddScoped<PermissionService>();
+
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
 
