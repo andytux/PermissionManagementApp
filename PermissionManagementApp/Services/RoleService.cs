2d1e5a9f68f458f2d954a6811941687f16b0f4b9
Andy <andy-tux@aon.at>
2025-02-27 11:17:13 +0100

    Added RoleService

---
+++ PermissionManagementApp/Services/RoleService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PermissionManagementApp.Data;
+using PermissionManagementApp.Data.Models;
+
+namespace PermissionManagementApp.Services
+{
+	public class RoleService
+	{
+		private readonly AppDbContext dbContext;
+
+		public RoleService(AppDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		public async Task<List<Permission>> GetUserPermissionsAsync(int userId)
+		{
+			var userPermission = await dbContext.UserPermissions
+				.Where(up => up.UserId == userId)
+				.Select(up => up.Permission)
+				.ToListAsync();
+
+			var groupPermission = await dbContext.UserGroups
+				.Where(ug => ug.UserId == userId)
+				.SelectMany(ug => dbContext.GroupPermissions.Where(gp => gp.GroupId == ug.GroupId).Select(gp => gp.Permission))
+				.ToListAsync();
+		}
+
+		public async Task<bool> HasPermissionAsync(int userId, string permissionName)
+		{
+			var permissions = await GetUserPermissionsAsync(userId);
+			return permissions.Any(p=> p.Name == permissionName);
+		}
+	}
+}
